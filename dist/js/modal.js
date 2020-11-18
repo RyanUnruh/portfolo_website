0a7a2a7f4f4243a2bb227c1ab71d0beb6fbf1f7e
ryanunruh <ryanunruhmedia@gmail.com>
2020-11-18 16:15:12 -0600

    Modals created for each project

--- dist/js/modal.js
+++ dist/js/modal.js
@@ -1,44 +1,54 @@
-// Open the Modal
+// Open the Modal 1
 function openModal() {
   document.getElementById("myModal").style.display = "block";
 }
 
-// Close the Modal
+// Close the Modal1
 function closeModal() {
   document.getElementById("myModal").style.display = "none";
 }
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+// Open the Modal2
+function openModal2() {
+  document.getElementById("myModal2").style.display = "block";
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
+// Close the Modal2
+function closeModal2() {
+  document.getElementById("myModal2").style.display = "none";
+}
+// Open the Modal3
+function openModal3() {
+  document.getElementById("myModal3").style.display = "block";
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
+// Close the Modal3
+function closeModal3() {
+  document.getElementById("myModal3").style.display = "none";
+}
+// Open the Modal4
+function openModal4() {
+  document.getElementById("myModal4").style.display = "block";
+}
+
+// Close the Modal4
+function closeModal4() {
+  document.getElementById("myModal4").style.display = "none";
+}
+// Open the Modal5
+function openModal5() {
+  document.getElementById("myModal5").style.display = "block";
+}
+
+// Close the Modal5
+function closeModal5() {
+  document.getElementById("myModal5").style.display = "none";
+}
+// Open the Modal
+function openModal6() {
+  document.getElementById("myModal6").style.display = "block";
+}
+
+// Close the Modal6
+function closeModal6() {
+  document.getElementById("myModal6").style.display = "none";
 }
