7b42464d402b08ecfd7db283efba219471385a61
ryanunruh <ryanunruhmedia@gmail.com>
2020-12-04 13:27:18 -0600

    Fade out fix. Rid of JQuery

--- dist/js/app.js
+++ dist/js/app.js
@@ -126,8 +126,26 @@ window.onload = function () {
   });
 };
 
-// Preloader
-$(window).load(function () {
-  // Animate loader off screen
-  $(".se-pre-con").fadeOut("slow");
+// // Preloader
+// $(window).load(function () {
+//   // Animate loader off screen
+//   $(".se-pre-con").fadeOut("slow");
+// });
+
+function fadeOutEffect() {
+  var fadeTarget = document.getElementById("target");
+  var fadeEffect = setInterval(function () {
+    if (!fadeTarget.style.opacity) {
+      fadeTarget.style.opacity = 1;
+    }
+    if (fadeTarget.style.opacity > 0) {
+      fadeTarget.style.opacity -= 0.08;
+    } else {
+      clearInterval(fadeEffect);
+    }
+  }, 20);
+}
+
+window.addEventListener("load", (event) => {
+  fadeOutEffect();
 });
